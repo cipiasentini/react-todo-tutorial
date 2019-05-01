c40aafb69de331e838164433cebb8225df1739f3
Carlos Piasentini <cipiasentini@ca.frre.utn.edu.ar>
2019-05-01 16:45:36 -0300

    as

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './App.css';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import MainContent from '../components/MainContent';
+//
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//           <Header />
+//           <MainContent />
+//           <Footer />
+//       </div>
+//     )
+//   }
+// }
+function App() {
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+    )
+}
+
+export default App;
